7d8c11bfb2fe5195cfa672b6603b7fc81de7fa54
Manchem <viswanadhammanchem13@gmail.com>
2025-05-17 23:29:38 +0530

    Shell Script Started

---
+++ Variables1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+Person1=$Trump
+Person2=Mask
+echo "$Person1:: Hey, $Person2 How are you?"
+echo "$Person2:: Hello $Person1. I am fine, how are you doing"
+echo "$Person1:: I am fine too. What's Up?"
+echo "$Person2:: Nothing Just going Mars now, Will you come?"
+echo "$Person1:: Sorry. Carry on, I will come once you are come back"
