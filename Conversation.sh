a13af4a05723e6c6eb043bb81829dab95df1d9d2
Manchem <viswanadhammanchem13@gmail.com>
2025-05-17 23:18:57 +0530

    Shell Script Started

---
+++ Conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Tump:: Hey, Mask How are you?"
+echo "Mask:: Hello Trump. I am fine, how are you doing"
+echo "Trump:: I am fine too. What's Up?"
+echo "Mask:: Nothing Just going Mars now, Will you come?"
+echo "Trump:: Sorry. Carry on, I will come once you are come back"
