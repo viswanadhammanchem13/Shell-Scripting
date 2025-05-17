0ac68ce135920277e185ca1eaab9f34b2d7fc18a
Manchem <viswanadhammanchem13@gmail.com>
2025-05-17 23:37:52 +0530

    Shell Script Started

---
+++ Varaible2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+Person1=$1 # No Space after Equal
+Person2=$2
+echo "$Person1:: Hey, $Person2 How are you?"
+echo "$Person2:: Hello $Person1. I am fine, how are you doing"
+echo "$Person1:: I am fine too. What's Up?"
+echo "$Person2:: Nothing Just going Mars now, Will you come?"
+echo "$Person1:: Sorry. Carry on, I will come once you are come back
