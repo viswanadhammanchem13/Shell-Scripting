0c5e618da5fd25eb4e56f20b40d8e243d3cc1e1f
Manchem <viswanadhammanchem13@gmail.com>
2025-05-24 07:55:13 +0530

    Shell Script Started

--- Datatypes01.sh
+++ Datatypes01.sh
@@ -2,5 +2,5 @@
 
 Number1=100 #Var1
 Number2=200 #Var2
-Sum=$(($Number1+$Number2)) #Var3 and result will store in Sum Variabel
+Sum=$(($Number1+$Number2)) #Var3 $((+)--> behave as Operator)--> Store the O/P and result will store in Sum Variabel
 echo "Sum of two digits is=$Sum"
