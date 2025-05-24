d74a4ef66ab57d77eec3467a69a96696f6c95381
Manchem <viswanadhammanchem13@gmail.com>
2025-05-24 07:16:05 +0530

    Shell Script Started

--- Datatypes03.sh
+++ Datatypes03.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-Number1=Viswanadham #Var1
+Number1=Viswanadham #Var1 and considering the letter as 0
 Number2=200 #Var2
 Time_Taken=$(date) #Date command executed and result will store in Time_Taken Variabel
 echo "Script executed at $Time_Taken" #printing the Variabe
