1bb93b5e9eaaa28dd9f8a2662701b1ae8465c66f
Manchem <viswanadhammanchem13@gmail.com>
2025-05-24 23:30:03 +0530

    Shell Script Started

--- Special_Var2.sh
+++ Special_Var2.sh
@@ -7,5 +7,6 @@ echo "Present Working Directory:$PWD"
 echo "Home Directory:$Home"
 echo "Which User is running:$USER"
 echo "PID of the current Script:$$"
+sleep 10 &
 echo "PID of the last command running in background:$!"
 echo "The exit status of the last command:$?"
