6acba67d02445765e6767f6c846ca11e55e9daf1
Manchem <viswanadhammanchem13@gmail.com>
2025-05-24 23:19:16 +0530

    Shell Script Started

--- Special_Var.sh
+++ Special_Var.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-echo "All Varaibles passed to the script is:#@"
-echo "Number of Variables Passed:@#"
+echo "All Varaibles passed to the script is:$@"
+echo "Number of Variables Passed:$#"
