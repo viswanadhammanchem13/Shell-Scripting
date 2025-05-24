cfb842e1253b19a940f1aab0e9960f6a0210c7b1
Manchem <viswanadhammanchem13@gmail.com>
2025-05-24 07:12:57 +0530

    Shell Script Started

--- Datatypes02.sh
+++ Datatypes02.sh
@@ -2,7 +2,7 @@
 
 Number1=100 #Var1
 Number2=200 #Var2
-Time_Taken=$(date)
-echo "Script executed at $Time_Taken"
-Sum=$(($Number1+$Number2)) #Var3 and result will store in Sum Variabel
-echo "Sum of two digits is=$Sum"
+Time_Taken=$(date)#Date command executed and result will store in Time_Taken Variabel
+echo "Script executed at $Time_Taken" #printing the Variabe
+Sum=$(($Number1+$Number2)) #Sum Variabel executed and result will store in Sum Variabel
+echo "Sum of two digits is=$Sum" #Printing the Sum
