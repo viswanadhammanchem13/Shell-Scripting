22b94779b573b6c63af12cbc782238df91a53553
Manchem <viswanadhammanchem13@gmail.com>
2025-05-24 07:45:19 +0530

    Shell Script Started

--- Array02.sh
+++ Array02.sh
@@ -6,4 +6,4 @@ Movie=("Court" "HIT3" "Puspha2" "Thandel")
 echo "First Movie of: ${Movie[0]}" #${} We saying to consider as whole varabel
 echo "third Movie of: ${Movie[3]}"
 echo "All Movies of: ${Movie[@]}"
-echo "Fifth Movie of: ${Movie[5]}"
+echo "Fifth Movie of: ${Movie[5]}" # It will the O/P as blank
