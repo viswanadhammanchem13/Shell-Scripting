79dc32fc912320ee08c451661f60bb1aca5b8165
Manchem <viswanadhammanchem13@gmail.com>
2025-05-24 07:39:03 +0530

    Shell Script Started

--- Array02.sh
+++ Array02.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 Movie=("Court" "HIT3" "Puspha2" "Thandel")
-echo "First Movie of: ${Movie[0]})" #Index 0 also consedering
+echo "First Movie of: ${Movie[0]}" #Index 0 also consedering
