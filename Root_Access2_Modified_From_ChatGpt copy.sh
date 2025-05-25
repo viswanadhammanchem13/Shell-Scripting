6c95805b86138df72764554204a7b7ca2cc1a6c0
Manchem <viswanadhammanchem13@gmail.com>
2025-05-25 08:17:21 +0530

    Shell Script Started

---
+++ Root_Access2_Modified_From_ChatGpt copy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ "$USERID" -ne 0 ]; then
+    echo "You do not have root access. Please run this script as root."
+    exit 1
+else
+    echo "You have root access. Proceeding to install MySQL Agent..."
+fi
+
+dnf install mysql -y
+if [ $? eq 0 ]
+then
+   echo "Installed MySql...Success"
+   exit 0
+else
+   echo "MySql Installation is failed"
+   exit 1
+fi
