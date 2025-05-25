e68236dfc846062991ef2e322e982431a4adc687
Manchem <viswanadhammanchem13@gmail.com>
2025-05-25 07:57:15 +0530

    Shell Script Started

--- Root_Access.sh
+++ Root_Access.sh
@@ -4,7 +4,6 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
     echo "You Have Root Access"
-    exit 0 # If it's have Root Acces
 else
     echo "You don't have Root have Access to install My Sql Agent"
     exit 1 # If it's don't have root access
