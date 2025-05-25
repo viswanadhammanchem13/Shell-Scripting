e8362f9deb3a9ce782628fc6841851d5603830ea
Manchem <viswanadhammanchem13@gmail.com>
2025-05-25 10:22:39 +0530

    Shell Script Started

---
+++ Functions.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+USERID=$(id -u) #Checks userId
+
+if [ "$USERID" -ne 0 ]; then
+    echo "You do not have root access. Please run this script as root."
+    exit 1
+else
+    echo "You have root access. Proceeding to install MySQL Agent..."
+fi
+Validate(){
+    if [ $? -eq 0 ]
+   then
+      echo "Installed MySql...Success"
+   else
+      echo "MySql Installation is failed"
+      exit 1
+    fi
+}
+dnf list installed mysql #Checks MySQL Installed or not, $? Should be equal to zero then only My Sql Installation is succussful
+if [ $? -ne 0] #If $? Not equal to Zero then it will install SQL
+then
+   echo " My Sql is not installed-----Going to install"
+   dnf install mysql -y #Install My Sql
+   Validate $? "MySql"  
+else
+   echo " My Sql Already Installed"
+   exit 1
+fi
