52b28d64bcb72b769cf971913d291a376cfdc0e5
Manchem <viswanadhammanchem13@gmail.com>
2025-05-25 08:35:44 +0530

    Shell Script Started

---
+++ Checking_Package_Installed_Or_Not.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ "$USERID" -ne 0 ]; then
+    echo "You do not have root access. Please run this script as root."
+    exit 1
+else
+    echo "You have root access. Proceeding to install MySQL Agent..."
+fi
+dnf list installed mysql
+if [ $? -ne 0]
+then
+   echo " My Sql is not installed-----Going to install"
+else
+   echo " My Sql Already Installed"
+fi
+dnf install mysql -y
+if [ $? -eq 0 ]
+then
+   echo "Installed MySql...Success"
+else
+   echo "MySql Installation is failed"
+   exit 1
+fi
